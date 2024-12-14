31a8f82272a1f8c0779f3fc2ae94c5227ec121d9
Rohit Gehlot <rohitgehlot5621@gmail.com>
2024-12-15 01:09:18 +0530

    Styling the clock

---
+++ app.js
@@ -0,0 +1,5 @@
+let clockSecond = document.querySelector("#second");
+let clockMinute = document.querySelector("#minute");
+let clockHour = document.querySelector("#hour");
+let clockAmPm = document.querySelector(".AM-PM");
+
